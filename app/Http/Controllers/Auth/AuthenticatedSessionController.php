5a6ea3c23915c29b859cc0ff5a8b901caf4d247e
michelange2008 <michelange@wanadoo.fr>
2024-04-01 17:29:12 +0200

    modif gitignire public/build

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -28,7 +28,7 @@ class AuthenticatedSessionController extends Controller
 
         $request->session()->regenerate();
 
-        return redirect()->intended(route('partie', absolute: false));
+        return redirect()->intended(route('jeux', absolute: false));
     }
 
     /**

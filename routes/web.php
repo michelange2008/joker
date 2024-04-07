6d5d82c04a5ddf789ce4ba20655a73cab51692d6
michelange2008 <michelange@wanadoo.fr>
2024-04-07 17:16:12 +0200

    modif web

--- routes/web.php
+++ routes/web.php
@@ -7,11 +7,8 @@ use App\Livewire\Partie;
 use App\Models\Jeu;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', Jeux::class)->name('partie');
-// Route::get('/', Partie::class)->name('partie');
-
-Route::middleware(['auth', 'verified'])->get('/', Jeux::class)->name('jeux');
-// Route::middleware(['auth', 'verified'])->get('/cartes', Cartes::class)->name('cartes');
+Route::get('/', Jeux::class)->name('jeux');
+// Route::middleware(['auth', 'verified'])->get('/', Jeux::class)->name('jeux');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
